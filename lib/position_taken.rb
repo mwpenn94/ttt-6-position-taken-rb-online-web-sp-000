32372f40c23cac55ec22bf274de0632383765af1
Michael Penn <mwpenn94@gmail.com>
2020-12-07 21:35:25 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,7 @@
-# code your #position_taken? method here!
+def position_taken?(board, index) # code your #position_taken? method here!
+if board[index] == "X" || board[index] == "O"
+  return TRUE
+elsif board[index] == " " || "" || nil
+  return FALSE
+end
+end
