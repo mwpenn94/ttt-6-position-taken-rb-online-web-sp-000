db5d7426f14f7ceb8bd4c6ea083d1d96caf5cc0e
Michael Penn <mwpenn94@gmail.com>
2020-12-07 21:32:43 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 def position_taken?(board, index) # code your #position_taken? method here!
-if board[index] == " " || "" || nil
-  return FALSE
-elsif board[index] == "X" || board[index] == "O"
+if board[index] == "X" || board[index] == "O"
   return TRUE
+elsif board[index] == " " || "" || nil
+  return FALSE
 else return TRUE
 end
 end
