9510d3300fd45f587476775914bf24afd1914368
Michael Penn <mwpenn94@gmail.com>
2020-12-07 21:20:03 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,6 +2,6 @@ def position_taken?(board, index) # code your #position_taken? method here!
 if board[index] == " " || "" || nil
   return FALSE
 elsif baord[index] == "X" || "x" || "O" || "o" 
-return TRUE
+  return TRUE
 end
 end
